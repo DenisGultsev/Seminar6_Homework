3903e74d750e892a8a3afa801c56e9462def8f89
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-08-09 22:32:45 +1000

    Task 001 Counting Numbers

---
+++ Task001_Counting_Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа через пробел:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int count = 0;
+        foreach (string number in numbers)
+        {
+            int num = Int32.Parse(number.Trim());
+            if (num > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел больше 0: " + count);
+    }
+}
